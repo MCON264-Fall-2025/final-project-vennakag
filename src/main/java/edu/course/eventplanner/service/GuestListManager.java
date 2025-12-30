d31608d092430972f1e1247bcb3a931f74085a1b
vennakag <vennakag@student.touro.edu>
2025-12-30 17:19:08 -0500

    Version 1
    Final project

--- src/main/java/edu/course/eventplanner/service/GuestListManager.java
+++ src/main/java/edu/course/eventplanner/service/GuestListManager.java
@@ -1,13 +1,27 @@
 package edu.course.eventplanner.service;
 
 import edu.course.eventplanner.model.Guest;
+import edu.course.eventplanner.util.Generators;
+
 import java.util.*;
 
 public class GuestListManager {
     private final LinkedList<Guest> guests = new LinkedList<>();
     private final Map<String, Guest> guestByName = new HashMap<>();
-    public void addGuest(Guest guest) { /* TODO */ }
-    public boolean removeGuest(String guestName) { return false; }
+    public GuestListManager(List<Guest> guests) {
+        for (Guest guest : guests) {
+            this.guests.add(guest);
+        }
+    }
+    public void addGuest(Guest guest) {
+        guests.add(guest);
+        guestByName.put(guest.getName(), guest);
+         }
+    public boolean removeGuest(String guestName) {
+        if(guestByName.containsKey(guestName)){
+
+        }
+        return false; }
     public Guest findGuest(String guestName) { return null; }
     public int getGuestCount() { return guests.size(); }
     public List<Guest> getAllGuests() { return guests; }

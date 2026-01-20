e0a17440f042dce93b8c83143b680eeeff514f65
vennakag <vennakag@student.touro.edu>
2026-01-19 19:59:20 -0500

    Last Commit

--- src/main/java/edu/course/eventplanner/service/VenueSelector.java
+++ src/main/java/edu/course/eventplanner/service/VenueSelector.java
@@ -7,16 +7,31 @@ public class VenueSelector {
     private final List<Venue> venues;
     public VenueSelector(List<Venue> venues) { this.venues = venues; }
     public Venue selectVenue(double budget, int guestCount) {
+        TreeMap<Double, List<Venue>> validOptions = new TreeMap<>();
         Venue selectedVenue = null;
-        for (Venue venue : venues) {
-            if(venues.isEmpty()) {
-                return null;
-            }else if(venue.getCost() <= budget && venue.getCapacity() >= guestCount) {
-                selectedVenue = venue;
-            }
-            if(selectedVenue != null){
-                return selectedVenue;
+        if (venues.isEmpty()) {
+            return null;
+        } else {
+            for (Venue venue : venues) {
+                if (venue.getCost() <= budget && venue.getCapacity() >= guestCount) {
+                    if(validOptions.containsKey(venue.getCost())) {
+                        validOptions.get(venue.getCost()).add( venue);
+                    }else {
+                        validOptions.put(venue.getCost(), new ArrayList<>());
+                        validOptions.get(venue.getCost()).add(venue);
+                    }
+                }
             }
         }
-        return null; }
+        if (validOptions.isEmpty()) {
+            return null;
+        }
+        Map.Entry<Double, List<Venue>> venues = validOptions.firstEntry();
+        for(Venue venue : venues.getValue()) {
+            if (selectedVenue == null || venue.getCapacity() < selectedVenue.getCapacity()) {
+                selectedVenue = venue;
+            }
+        }
+        return selectedVenue;
+    }
 }
